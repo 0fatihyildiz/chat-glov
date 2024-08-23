9179ee60af5e9767e9e724c8843033bac5ca526b
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2024-08-23 18:40:11 +0300

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+	stylistic: {
+		indent: 'tab',
+		quotes: 'single',
+		semi: false,
+	},
+	rules: {
+		'no-trailing-spaces': ['error', { skipBlankLines: true, ignoreComments: true }],
+		'no-console': 0,
+		'node/prefer-global/process': 0,
+		'no-undef': 'off',
+	},
+})
